63286f0ae695553108116ef585fd73cd38c4139c
jpVinnie <janpaul.ramos@protonmail.com>
2023-06-18 15:02:26 -0400

    update pipeline

---
+++ src/main/scala/gvteal/typing/Inference.scala
@@ -0,0 +1,2 @@
+package gvteal.typing
+// [Inference Algorithm](https://www.cs.cornell.edu/courses/cs3110/2011sp/Lectures/lec26-type-inference/type-inference.htm)
---
+++ src/main/scala/gvteal/typing/TypedAST.scala
@@ -0,0 +1,3 @@
+package gvteal.typing
+import gvteal.parser._
+
